47b0697bb11c80a88238b1a302e3637c45987a36
苏剑林 <bojone@spaces.ac.cn>
2018-07-28 20:44:46 +0800

    Update margin_softmax.py

--- margin_softmax.py
+++ margin_softmax.py
@@ -1,2 +1,6 @@
 #! -*- coding: utf-8 -*-
 
+
+# 普通sparse交叉熵，以logits为输入
+def sparse_logits_categorical_crossentropy(y_true, y_pred):
+    return K.sparse_categorical_crossentropy(y_true, y_pred, from_logits=True)
